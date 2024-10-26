6a80901627dbf232347f0c84063b81bd891227a1
unknown <geethmadushankagamage@gmail.com>
2024-10-26 23:50:29 +0200

    add unit test for controller

--- src/test/java/com/stackstitch/docker/controller/UserControllerTest.java
+++ src/test/java/com/stackstitch/docker/controller/UserControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ public class UserControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @Autowired
+    @MockBean
     UserService userService;
 
     @Test
@@ -30,6 +31,22 @@ public class UserControllerTest {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/user/getUsers/1")
                         .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Geeth"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Gamage"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.address").value("Helsingborg, Sweden"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.telephoneNo").value("0720400780"));
+    }
+
+    @Test
+    public void testAddUser() throws Exception {
+        UserDto userDto = new UserDto(1L, "Geeth", "Gamage", "Helsingborg, Sweden", "0720400780");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/addUser")
+                        .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"firstName\": \"John\", \"lastName\": \"Gamage\", \"address\": \"Helsingborg\", \"telephoneNo\": \"0720400780\"}"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
+
     }
 }
